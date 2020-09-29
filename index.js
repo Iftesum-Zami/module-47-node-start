b5aeefb5a8cf9e81b78b78d650993ce07ef4a3ed
Iftesum-Zami <tmizami20@gmail.com>
2020-09-29 23:17:30 +0600

    done

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json())
+
+const users = ['zami', 'zamil', 'nasif', 'sonet', 'dip', 'dipu'];
+
 app.get('/', (req, res) => {
-    res.send('thank u very much...yay');
+    const fruit = {
+        product: 'ada',
+        price: 220
+    }
+    res.send(fruit);
+})
+
+app.get('/fruits/banana', (req, res) => {
+    res.send({fruit: 'banana', quantity: 1000, price: 10000});
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;  // for reading ID
+    const name = users[id]; 
+    res.send({id, name});
+    
+    console.log(req.query);
+    console.log(req.query.sort);
+})
+
+// post
+app.post('/addUser', (req, res) => {
+    // save to database
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
 })
 
 app.listen(3000, () => console.log('listening to port 3000'));
